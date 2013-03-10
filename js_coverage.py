c1d6b27bebe94a5f568bf3985aafbacd99fe3919
Dyego Costa <dyego.costa@live.com>
2013-03-10 20:46:25 -0300

    Notes of what it should do

--- js_coverage.py
+++ js_coverage.py
@@ -8,7 +8,16 @@ DEFAULT_IS_ENABLED = False
 settings = sublime.load_settings('js_coverage.sublime-settings')
 plugin_enabled = bool(settings.get('js_coverage_enabled', DEFAULT_IS_ENABLED))
 
-def highlight_lines(view):
+def cover(view):
+    ## run_all_tests()
+
+    ## log = read_coverage_log()
+
+    ## highlight_uncovered_lines(log)
+
+    ## highlight_covered_lines(log)
+
+
     regions = view.lines(sublime.Region(0, view.size()))
     
     color_scope_name = settings.get('highlight_color',
@@ -18,29 +27,23 @@ def highlight_lines(view):
                         regions, color_scope_name,
                         sublime.DRAW_OUTLINED)
 
-def downlight_lines(window):
+def uncover(window):
     for view in window.views():
         view.erase_regions('JsCoverageListener')
 
 class JsCoverageCommand(sublime_plugin.WindowCommand):
     def run(self):
-        if plugin_enabled:
-            highlight_lines(self.window.active_view())
-        else:
-            downlight_lines(self.window)
+        cover(self.window.active_view()) if plugin_enabled else uncover(self.window)
 
 class JsCoverageListener(sublime_plugin.EventListener):
     def on_modified(self, view):
-        if plugin_enabled:
-            highlight_lines(view)
+        if plugin_enabled: cover(view)
 
     def on_activated(self, view):
-        if plugin_enabled:
-            highlight_lines(view)
+        if plugin_enabled: cover(view)
 
     def on_load(self, view):
-        if plugin_enabled:
-            highlight_lines(view)
+        if plugin_enabled: cover(view)
 
 class ToggleJsCoverageCommand(sublime_plugin.WindowCommand):
     def run(self):
@@ -48,6 +51,6 @@ class ToggleJsCoverageCommand(sublime_plugin.WindowCommand):
         plugin_enabled = False if plugin_enabled else True
 
         if plugin_enabled:
-            highlight_lines(self.window.active_view())
+            cover(self.window.active_view())
         else:
-            downlight_lines(self.window)
+            uncover(self.window)
