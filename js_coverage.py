3a46fa29ebeb9975b27b8c6eb2f5378293b40472
Dyego Costa <dyego.costa@live.com>
2013-03-10 20:27:19 -0300

    Refactoring to use region lines directly

--- js_coverage.py
+++ js_coverage.py
@@ -3,32 +3,19 @@ import sublime_plugin
 from collections import defaultdict
 
 DEFAULT_COLOR_SCOPE_NAME = "invalid"
-DEFAULT_IS_ENABLED = True
+DEFAULT_IS_ENABLED = False
 
 settings = sublime.load_settings('js_coverage.sublime-settings')
 plugin_enabled = bool(settings.get('js_coverage_enabled', DEFAULT_IS_ENABLED))
 
 def highlight_lines(view):
-    lines = view.lines(sublime.Region(0, view.size()))    
+    regions = view.lines(sublime.Region(0, view.size()))
     
-    counts = defaultdict(list)
-    for line in lines:
-        string = view.substr(line).strip()
-        counts[string].append(line)
-
-    show_lines(counts.values(), view)
-
-def show_lines(regions, view):
-    all_regions = []
-
-    for r in regions:
-        all_regions.extend(r)
-
     color_scope_name = settings.get('highlight_color',
                                         DEFAULT_COLOR_SCOPE_NAME)
 
     view.add_regions('JsCoverageListener',
-                        all_regions, color_scope_name,
+                        regions, color_scope_name,
                         sublime.DRAW_OUTLINED)
 
 def downlight_lines(window):
